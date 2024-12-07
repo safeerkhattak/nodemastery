b1da9c7569c7d97db1ac105a9b043ba4f97c27e8
safeer5454 <safeerahmad5454@gmail.com>
2024-12-08 02:36:56 +0500

    node block code example

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+
+const app = express();
+/*
+Real life blocking functions.
+JSON.stringify() if many requests are using the method then this will make the app to respond slowly
+JSON.parse()
+array.sort() if array is large
+*/
+
+function delay(duration) {
+    const startTime = Date.now()
+    while (Date.now() - startTime < duration) {// this will block the event loop, and app will not be able to handle the other requests untill this unfreeze the event loop
+    }
+}
+
+
+app.get('/', (req, res) => {
+    return res.send("hello world")
+})
+
+app.get('/blockReq', (req, res) => {
+    delay(9000)
+    return res.send("hello world")
+})
+
+app.listen(3000)
